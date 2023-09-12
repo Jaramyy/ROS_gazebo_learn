f0091a2331d02517cf73cbf5f4f355de4a6a7309
jaramyy <jamephet@gmail.com>
2023-09-12 20:48:32 +0700

    first commit

---
+++ build/catkin_tools_prebuild/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/jaramy/ros_gazebo/devel/.private/catkin_tools_prebuild:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/home/jaramy/bobo_ws/devel/lib:/opt/ros/noetic/lib:/opt/ros/noetic/lib/x86_64-linux-gnu:/usr/local/cuda-11.8/lib64'
+export PATH='/opt/ros/noetic/bin:/usr/local/cuda-11.8/bin:/home/jaramy/.local/bin:/opt/gcc-arm-none-eabi-9-2020-q2-update/bin:/home/jaramy/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PWD='/home/jaramy/ros_gazebo/build/catkin_tools_prebuild'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/jaramy/ros_gazebo/devel/.private/catkin_tools_prebuild/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/jaramy/ros_gazebo/build/catkin_tools_prebuild:$ROS_PACKAGE_PATH"
